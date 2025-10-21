09ac3788fd6dc602326d20e32673c682313bd85c
TCRD <tcrdinesh1@gmail.com>
2025-10-21 18:18:14 +0530

    remove duplicate variable file

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "gke_networking_mode" {
-  description = "Networking mode for GKE cluster"
-  type        = string
-}
-
-variable "gke_private_cluster_master_ipv4_cidr_block" {
-  description = "Master IPv4 CIDR block for private GKE cluster"
-  type        = string
-}
